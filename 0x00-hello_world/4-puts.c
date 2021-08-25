43cdfe44738c10dfa81bda739fff811cc6c5a6d3
Pericles001 <periclesadjovi@outlook.fr>
2021-08-26 00:36:21 +0100

    Add solution for challenge 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+
+char displayed[52];
+strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+puts(displayed);
+return (0);
+}

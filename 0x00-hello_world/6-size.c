18b3cb56fc30d9689ec98b2b889d163b0e71ea43
Pericles001 <periclesadjovi@outlook.fr>
2021-08-26 02:54:04 +0100

    Add solution to challenge 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+/**
+*main - main function
+*
+*
+*Return: return 0
+*/
+int main(void)
+{
+printf("Size of a char:%lu\n", sizeof(char));
+printf("Size of an int:%lu\n", sizeof(int));
+printf("Size of a long int:%lu\n", sizeof(long));
+printf("Size of a long long int:%lu\n", sizeof(long));
+printf("Size of a float:%lu\n", sizeof(float));
+return (0);
+}

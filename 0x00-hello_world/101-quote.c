0bbb560f38c9abeca57496def2ee8b2c0260c457
Pericles001 <periclesadjovi@outlook.fr>
2021-08-26 05:16:32 +0100

    Change content length

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 30);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }

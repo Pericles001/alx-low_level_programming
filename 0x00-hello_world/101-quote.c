c077f71a50cede4b0d5708b1d7556269ac28fa9f
Pericles001 <periclesadjovi@outlook.fr>
2021-08-26 04:56:03 +0100

    Resolve challenge 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/**
+*main - main function in the task
+*
+*
+*Return: return 1
+*/
+int main(void)
+{
+fprintf(stderr, "%s", "and that piece of art is useful\"
+- Dora Korpar, 2015-10-19\n");
+return (1);
+}

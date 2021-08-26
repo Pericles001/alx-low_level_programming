6a5f328b9437d66a3eab15129a16b7d5b21f3566
Pericles001 <periclesadjovi@outlook.fr>
2021-08-26 01:09:37 +0100

    Add solution to challenge 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+*main - Main function
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

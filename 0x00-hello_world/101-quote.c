33b2f82948e420cdc79e13c06e640e1f73580ceb
Pericles001 <periclesadjovi@outlook.fr>
2021-08-26 05:12:59 +0100

    Change file structure

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include <unistd.h>
 /**
 *main - main function in the task
 *
@@ -7,7 +8,6 @@
 */
 int main(void)
 {
-fprintf(stderr,"%s", "and that piece of art is useful\"
- - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 30);
 return (1);
 }

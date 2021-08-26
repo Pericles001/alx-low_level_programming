080203e0d0ab3f03c9ecde89584cdaafdcf248fb
Pericles001 <periclesadjovi@outlook.fr>
2021-08-26 05:01:15 +0100

    Cut line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
 */
 int main(void)
 {
-fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr,"%s", "and that piece of art is useful\"
+ - Dora Korpar, 2015-10-19\n");
 return (1);
 }

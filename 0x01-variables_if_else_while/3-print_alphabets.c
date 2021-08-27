4a1ee47acad507b1447c4efa8a22a8f1c6cbd091
Pericles001 <periclesadjovi@outlook.fr>
2021-08-27 13:40:55 +0100

    Resolve letters printer challenge

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Program to print alphabet letters in all cases followed by new line
+*
+*Return: return 0
+*/
+int main(void)
+{
+int letter;
+for (letter = 'a'; letter <= 'z' ; letter++)
+{
+letter = tolower(letter);
+putchar(letter);
+if (letter == 'z')
+{
+letter = 'A';
+for (; letter <= 'Z'; letter++)
+{
+putchar(letter);
+}
+break;
+}
+}
+putchar('\n');
+return (0);
+}

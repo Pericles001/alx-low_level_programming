2455b939d82ca3066d2e581695fa2a4966723574
Pericles001 <periclesadjovi@outlook.fr>
2021-08-27 14:01:55 +0100

    Submit solution

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Program to print alphabet letters in reverse followed by new line
+*
+*Return: return 0
+*/
+int main(void)
+{
+int letter;
+for (letter = 'z'; letter >= 'a' ; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}

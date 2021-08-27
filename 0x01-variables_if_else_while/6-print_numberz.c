aecce53e3d94f89777f9ece02971249c8bdfc87d
Pericles001 <periclesadjovi@outlook.fr>
2021-08-27 13:59:42 +0100

    Submit solution

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Prints all single digits of base 10 followed by new line
+*
+*Return: returns 0
+*/
+int main(void)
+{
+int digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
+}

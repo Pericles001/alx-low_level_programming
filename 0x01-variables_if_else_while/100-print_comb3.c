4b71917d19d7e6aaad9c4e8a4541d575e27d4473
Pericles001 <periclesadjovi@outlook.fr>
2021-08-27 15:01:41 +0100

    Change code structure

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,15 @@
 int main(void)
 {
 int digit1, digit2;
-for (digit1 = 0; digit1 < 10; digit1++)
+for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2 = 1; digit2 < 10; digit2++)
+for (digit2 = digit1 + 1 ; digit2 < 10; digit2++)
 {
-
-if (digit1 == digit2)
-continue;
 
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 
-if (digit1 == 9 && digit2 == 9)
+if (digit1 == 8 && digit2 == 9)
 continue;
 
 putchar(',');

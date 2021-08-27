235b31a107fe14630e8dc35c557c653a48b99229
Pericles001 <periclesadjovi@outlook.fr>
2021-08-27 13:26:37 +0100

    Correct coding dtyle

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*Main function - Prints the last digit of random number
+*main - Prints the last digit of random number
 *
 *Return: Return 0
 */
@@ -15,19 +15,20 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 last = n % 10;
 
-if (last == 0) {
-  printf("Last digit of %d is %d and is 0\n",n , last );
+if (last == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, last);
 }
 else
 {
-  if (last > 5)
-  {
-    printf("Last digit of %d is %d and is greater than 5\n",n , last );
-  }
-  else if (last < 6)
-  {
-    printf("Last digit of %d is %d and is less than 6 and not 0\n",n , last );
-  }
+if (last > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
+}
+else if (last < 6)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+}
 
 }
 return (0);

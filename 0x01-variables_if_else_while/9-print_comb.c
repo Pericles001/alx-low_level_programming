24ea9963d4f277252d291c2533c301d65bf851ac
Pericles001 <periclesadjovi@outlook.fr>
2021-08-27 14:25:07 +0100

    Submit solution

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Prints all single digits of base 10 followed by new line
+*
+*Return: returns 0
+*/
+int main(void)
+{
+int digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+if (digit == '9')
+continue;
+
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}

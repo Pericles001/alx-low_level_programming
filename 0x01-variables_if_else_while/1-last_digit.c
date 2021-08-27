0e90411daba6bdc3de12ee6ad9120681d619ae3e
Pericles001 <periclesadjovi@outlook.fr>
2021-08-27 01:07:24 +0100

    Resolve last digit challenge

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+*Main function - Prints the last digit of random number
+*
+*Return: Return 0
+*/
+int main(void)
+{
+int n;
+int last;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = n % 10;
+
+if (last == 0) {
+  printf("Last digit of %d is %d and is 0\n",n , last );
+}
+else
+{
+  if (last > 5)
+  {
+    printf("Last digit of %d is %d and is greater than 5\n",n , last );
+  }
+  else if (last < 6)
+  {
+    printf("Last digit of %d is %d and is less than 6 and not 0\n",n , last );
+  }
+
+}
+return (0);
+}

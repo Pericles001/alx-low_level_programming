0f984b0834973fe3e7ce2cb4cbc422687edc7075
Pericles001 <periclesadjovi@outlook.fr>
2021-08-27 14:07:22 +0100

    Submit solution

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - Prints all single digits of base 16 followed by new line
+*
+*Return: returns 0
+*/
+int main(void)
+{
+int digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+if (digit == '9')
+{
+digit = 'a';
+for (; digit <= 'f'; digit++)
+{
+putchar(digit);
+}
+break;
+}
+}
+putchar('\n');
+return (0);
+}

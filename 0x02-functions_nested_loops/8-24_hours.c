7e050963b4145c587f56eb6b7224a477d34d1d01
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 20:27:17 +0100

    Change some details

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-*24_hours - prints each minute of the day
+*jack_bauer - prints each minute of the day
 *
 *Return:returns 0
 */
 void jack_bauer(void)
 {
 int min, hour;
-for (hour = 0; hour < 23; hour++)
+for (hour = 0; hour <= 23; hour++)
 {
-for (min = 0; min < 59; min++)
+for (min = 0; min <= 59; min++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');

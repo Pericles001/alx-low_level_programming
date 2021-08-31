4f160521c35b949e91e622d9333711623277e6aa
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 00:54:34 +0100

    Add description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
 *add - sums up two numbers
-*
+*@dg1:first integer
+*@dg2:second integer
 *Return: returns 0
 */
 int add(int dg1, int dg2)
 {
-return (dg1 +dg2);
+return (dg1 + dg2);
 }

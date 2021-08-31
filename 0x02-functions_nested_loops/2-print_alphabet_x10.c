b45133559ecf8d2cb5011fbc53441a891ebd9d8f
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 16:27:20 +0100

    Add solution to task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+*Return: no return
+*/
+void print_alphabet_x10 (void)
+{
+int counter = 0;
+int letter;  
+while (counter <= 10)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar(10);
+counter += 1;
+}
+}

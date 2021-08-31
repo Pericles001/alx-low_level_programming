643d596cc95c3df77c3222185eed97d36ce8eac3
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 16:32:54 +0100

    Remove space between function name and parenthesis

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,9 @@
 *desc: The function uses loop to operate
 *Return: no return
 */
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 int counter = 0;
-int letter;
 while (counter <= 10)
 {
 print_alphabet();

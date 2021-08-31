6e0c024758ddc0b6293b3ff419e4a254cd573ef2
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 18:06:25 +0100

    Submit solution

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*print_last_digit - function that print last digit of a number
+*@i: number to be targeted
+*Return: returns 0
+*/
+int print_last_digit(int i)
+{
+return (i%10);
+}

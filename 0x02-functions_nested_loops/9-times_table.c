fdbfb137138cd922860ce68783db4d24068bd298
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 22:15:13 +0100

    Review function

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,12 +12,22 @@ for (digit = 0; digit <= 9; digit++)
 for (mult = 0;  mult <= 9; mult++)
 {
 result = digit * mult;
-if (result > 9) {
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
-}else
+if (result > 9)
+{
+ _putchar((result / 10) + '0');
+ _putchar((result % 10) + '0');
+}
+else
+{
+_putchar(' ');
 _putchar(result + '0');
+}
 
+if (mult == 9)
+{
+continue;
+}
+else
 _putchar(',');
 _putchar(' ');
 }

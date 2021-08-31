7f114f5801513ef92cb0b2dbb7f8953908ecbdcc
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 17:46:07 +0100

    Resolve coding style warnings

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_isalpha - controls if a character is alphabetical
+*_print_sign - controls if a character is alphabetical
 *@n: character to be verified
 *Return: return 0 or 1
 */
@@ -11,14 +11,14 @@ if (n == 0)
 _putchar('0');
 return (0);
 }
-else if ( n > 0)
+else if (n > 0)
 {
-  _putchar('+');
-  return (1);
+_putchar('+');
+return (1);
 }
 else
 {
-  _putchar('-');
-  return (-1);
+_putchar('-');
+return (-1);
 }
 }

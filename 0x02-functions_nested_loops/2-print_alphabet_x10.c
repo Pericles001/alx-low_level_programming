1ff9531a959fa74325f825c0b1aff4c486e72610
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 16:35:58 +0100

    Change counter iteration

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,14 @@
 void print_alphabet_x10(void)
 {
 int counter = 0;
+int letter;
 while (counter <= 10)
 {
-print_alphabet();
-counter += 1;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar(10);
+counter ++;
 }
 }

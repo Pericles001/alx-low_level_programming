f161e3c88d8b147060e91fa0e1a08f740bb0d8d0
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 16:10:46 +0100

    Reduce code for solution

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
-void print_alphabet(void);
-/**
-*main - function that print alphabet letters
-*it uses another function call
-*Return: returns 0
-*/
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+#include "holberton.h"
 /**
 *print_alphabet - function that print alphabet
 *it prints in lowercase

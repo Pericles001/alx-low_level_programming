bca6e06e146451ebb055ee9c19e95dc2472a2d10
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 23:06:27 +0100

    Indent code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,22 +9,20 @@ void times_table(void)
 int digit, mult, result;
 for (digit = 0; digit <= 9; digit++)
 {
-  _putchar('0');
+_putchar('0');
 
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 9)
+_putchar(' ');
+else
+_putchar((result / 10) + '0');
 
-			result = digit * mult;
-
-			if (result <= 9)
-				_putchar(' ');
-			else
-				_putchar((result / 10) + '0');
-
-			_putchar((result % 10) + '0');
-		}
-		_putchar('\n');
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
 }
 }

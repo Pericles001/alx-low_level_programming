6769b07a60bf44f818bb0a5fa3e9d2a24130b402
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 20:22:57 +0100

    Add corrections

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,19 +4,19 @@
 *
 *Return:returns 0
 */
-void 24_hours (void)
+void 24_hours(void)
 {
 int min, hour;
-for (hour = 0; hour < 23; hour ++)
+for (hour = 0; hour < 23; hour++)
 {
 for (min = 0; min < 59; min++)
 {
-_putchar (hour / 10 + '0');
-_putchar (hour %10 + '0');
-_putchar (':');
-_putchar (min / 10 + '0' );
-_putchar (min %10 + '0');
-_putchar (10);
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar(10);
 }
 }
 

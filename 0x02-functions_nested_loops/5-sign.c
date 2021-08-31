815a4ba2b1b9969bb3aff1098a3623c017040ff2
Pericles001 <periclesadjovi@outlook.fr>
2021-08-31 17:31:05 +0100

    Submit solution

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_isalpha - controls if a character is alphabetical
+*@n: character to be verified
+*Return: return 0 or 1
+*/
+int print_sign(int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if ( n > 0)
+{
+  _putchar('+');
+  return (1);
+}
+else
+{
+  _putchar('-');
+  return (-1);
+}
+}

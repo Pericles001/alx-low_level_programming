85a0af8d96b03897d7dfea143f127019c290cf17
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 03:28:26 +0100

    Review code

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,15 +7,15 @@
 */
 int main(void)
 {
-unsigned long n1 = 0, n2 = 1, n3, sum;
-while (sum <= 4000000)
+unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+while (n3 <= 4000000)
 {
 n3 = n1 + n2;
 n1 = n2;
 n2 = n3;
 
-if ((n3 % 2) == 0)
-sum += n3;
+if ((n1 % 2) == 0)
+sum += n1;
 
 }
 printf("%ld\n", sum);

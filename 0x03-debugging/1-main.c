b24aa04d8797edae0590461ac4757441fee58893
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 20:27:58 +0100

    Review code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-while (i < 10)
-{
-/*putchar(i);*/
-}
-
+/*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);

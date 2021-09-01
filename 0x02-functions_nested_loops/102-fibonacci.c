0c0b65c202fcd5ba1ac5a6f05852bedb5f4216f7
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 03:00:22 +0100

    Change code structure

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,12 +11,15 @@ unsigned long n1 = 0, n2 = 1, n3;
 for (inc = 0; inc < 50; inc++)
 {
 n3 = n1 + n2;
-printf("%lu, ", n3);
+printf("%lu", n3);
 n1 = n2;
 n2 = n3;
 
 if (inc == 49)
-printf("%lu\n", n3);
+printf("\n");
+else
+printf(", ");
+
 }
 
 return (0);

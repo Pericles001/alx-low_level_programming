cd4347f47cd10c5c8aff66bd5ff1ae62c5bf501c
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 20:43:22 +0100

    Add equality sign

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,13 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if (a >= b && b >= c)
 {
 largest = a;
 }
-else if (b > a && a > c)
+else if (b >= a && a >= c)
 {
 largest = b;
-}
-else if ((a == b) && (b == c))
-{
-largest = a;
 }
 else
 {

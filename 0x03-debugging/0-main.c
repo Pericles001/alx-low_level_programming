515d5bf4dd702261772190619693ec4bf1e7d854
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 20:21:17 +0100

    Add files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,7 +8,9 @@
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int);
+void positive_or_negative(int i);
 
 #endif /* MAIN_H */

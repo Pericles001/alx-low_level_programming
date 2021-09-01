75db6780628eb79b2086d66b9d39689ed94d0e55
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 21:27:58 +0100

    Review code

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,6 @@
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
-void print_remaining_days(int month, int day, int year);
+void print_remaining_days(int, int, int);
 
 #endif /* MAIN_H */

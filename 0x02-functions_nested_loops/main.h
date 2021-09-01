8cf6c53a92509b63e92f0359049f52ab0ae51eb7
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 01:26:37 +0100

    Change prototype details

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,7 @@ void print_alphabet(void);
 void jack_bauer(void);
 void print_alphabet_x10(void);
 void times_table();
+void print_to_98(int);
 int _putchar(char);
 int _islower(int c);
 int _isalpha(int c);

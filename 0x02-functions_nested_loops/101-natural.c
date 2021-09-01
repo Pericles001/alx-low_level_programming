f1173062002c9848f08ffb2b5e1dc2d28a88a4ef
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 02:19:30 +0100

    Add betty style

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,14 +4,14 @@
 *of 3 or 5 below 1024
 *Return: return 0
 */
-int main (void)
+int main(void)
 {
 int n, sum = 0;
 for (n = 0; n < 1024; n++)
 {
 if ((n % 3) == 0 || (n % 5) == 0)
-sum += n ;
+sum += n;
 }
 printf("%d\n", sum);
-return 0;
+return (0);
 }

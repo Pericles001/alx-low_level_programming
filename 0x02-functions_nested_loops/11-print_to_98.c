9d5833bc27bc6fdcc3e5ebc2c2b1cead9d0e0acc
Pericles001 <periclesadjovi@outlook.fr>
2021-09-01 01:28:20 +0100

    Add comma and space

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,14 @@ void print_to_98(int n)
 if (n >= 98)
 {
 while (n > 98)
-printf("%d", n--);
+printf("%d, ", n--);
 
 printf("%d\n", n);
 }
 else
 {
 while (n < 98)
-printf("%d", n++);
+printf("%d, ", n++);
 
 printf("%d\n", n);
 }

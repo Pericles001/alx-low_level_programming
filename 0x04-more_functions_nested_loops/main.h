12d2f2bb8b1a83258f5ea7eb504d4b8845d6765a
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 00:17:17 +0100

    Add new prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,6 +9,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);

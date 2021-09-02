da684a357030c5df7f7f89d296d1480323721a77
Pericles001 <periclesadjovi@outlook.fr>
2021-09-02 21:10:57 +0100

    Submit solution

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+int number = 48;
+for (letter = 48; letter < 58; letter++)
+{
+_putchar(letter);
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
+*main - header file for protoypes
 *void protoypes (void)
 *int protoypes (int)
 */
-
+void print_numbers(void);
+int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);

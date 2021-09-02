a238577f16a5361b5782a1f5e209e2c521eaa019
Pericles001 <periclesadjovi@outlook.fr>
2021-09-02 23:12:55 +0100

    Remove equality marks

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 int number1, number2;
 
-for (number1 = 0; number1 <= 10; number1++)
+for (number1 = 0; number1 < 10; number1++)
 {
 for (number2 = 0; number2 <= 14; number2++)
 
@@ -17,7 +17,7 @@ if (number2 > 9)
 {
 _putchar((number2 / 10) + '0');
 }
-_putchar(number2 % 10 + '0');
+_putchar((number2 % 10) + '0');
 }
 _putchar(10);
 }

25af6b72b152994665b29d6bd95c6f8c8d2129e8
Pericles001 <periclesadjovi@outlook.fr>
2021-09-02 20:39:05 +0100

    Add function prototype

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,7 +7,7 @@
 
 int _isdigit(int c)
 {
-if ((c >= 48) && (c <= 58))
+if ((c >= 48) && (c <= 57))
 return (1);
 
 return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@
 */
 
 int _isupper(int);
+int _isdigit(int);
 
 #endif /* MAIN_H */

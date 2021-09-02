c1fcfcd610292f58810bb2eb959dfdeab0989d02
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 00:53:44 +0100

    Submit solution

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_square - prints squares
+*@size: parameter
+*Return:returns nothing
+*/
+void print_line(int size)
+{
+while (size-- > 0)
+{
+_putchar('#');
+}
+_putchar('\n');
+}

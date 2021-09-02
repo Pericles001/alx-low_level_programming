cd6a990d579594a07a4dc036d1e25e6f411233f4
Pericles001 <periclesadjovi@outlook.fr>
2021-09-02 23:19:53 +0100

    Change line 13

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@ int number1, number2;
 
 for (number1 = 0; number1 < 10; number1++)
 {
-for (number2 = 0; number2 < 14; number2++)
+for (number2 = 0; number2 <= 14; number2++)
 
 {
 if (number2 > 9)

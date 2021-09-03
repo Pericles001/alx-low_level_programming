6c6c255ef530129430948038b78e9b18850382b7
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 01:32:35 +0100

    Review code base

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,9 +6,18 @@
 */
 void print_square(int size)
 {
-while (size-- > 0)
+int inc1, inc2;
+if (size > 0)
 {
+for (inc1 = 0; inc1 < size; inc1++)
+{
+for (inc2 = 0; inc2 < (size - 1); inc2++)
 _putchar('#');
+
+_putchar('#');
+_putchar('\n');
+}
+
 }
 _putchar('\n');
 }

db2394fa66289347e4132f3bdbe27bf3577bee63
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 13:44:09 +0100

    Add new variable

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,15 @@
 */
 void print_number(int n)
 {
+unsigned int num = n;
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+num = -num;
 }
-if (n > 9)
+if (num > 9)
 {
-print_number(n / 10);
+print_number(num / 10);
 }
-_putchar(n % 10 + '0');
+_putchar(num % 10 + '0');
 }

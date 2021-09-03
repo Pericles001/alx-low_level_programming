a2f0211dae6bbb709574a46fd6dcf52fbc59c031
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 02:50:33 +0100

    Code review

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,19 @@ int num;
 while (num++ < 100)
 
 if ((num % 3 == 0) && (num % 5 == 0))
-{
-if (num != 100)
 printf("fizzBuzz ");
-
-else
-printf("fizzBuzz");
-}
 
 else if ((num % 3) == 0)
 printf("Fizz ");
 
 else if ((num % 5) == 0)
+{
+if (num != 100)
 printf("Buzz ");
+
+else
+printf("Buzz");
+}
 
 else
 printf("%d ", num);

6a2746f0655dc49d53395898677468e6c0eb6219
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 02:25:03 +0100

    Resolve errors

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,17 +15,16 @@ while (num++ < 100)
 if ((num % 3 == 0) && (num % 5 == 0))
 printf("fizzBuzz");
 
-else if (num % 3 == 0)
+else if ((num % 3) == 0)
 printf("Fizz ");
 
-else if (num % 5 == 0)
+else if ((num % 5) == 0)
 {
 if (num != 100)
 printf("Buzz ");
 
 else
 printf("Buzz");
-
 }
 
 else

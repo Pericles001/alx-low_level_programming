c98c6ab2fd1dfe22a441295d33221fd2dcb198f8
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 02:36:19 +0100

    Add space to fizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,13 @@ int num;
 while (num++ < 100)
 
 if ((num % 3 == 0) && (num % 5 == 0))
-printf("fizzBuzz");
+printf("fizzBuzz ");
 
 else if ((num % 3) == 0)
 printf("Fizz ");
 
 else if ((num % 5) == 0)
-{
-if (num != 100)
 printf("Buzz ");
-
-else
-printf("Buzz");
-}
 
 else
 printf("%d ", num);

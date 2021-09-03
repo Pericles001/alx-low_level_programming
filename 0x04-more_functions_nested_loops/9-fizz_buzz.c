d4d1adfc20adf6c9b73124af14c27b5748a7842c
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 02:05:03 +0100

    Submit solution

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - program that prints either number
+*or fizz or buzz or fizzBuzz
+*
+*
+*Return: returns 0
+*/
+int main(void)
+{
+int num;
+while(num++ < 100)
+
+if ((num % 3 == 0) && (num % 5 == 0))
+printf("fizzBuzz");
+
+else if (num % 3 == 0)
+printf("Fizz ");
+
+else if (num % 5 == 0)
+printf("Buzz ");
+
+else
+printf("%d ", num );
+printf("\n");
+}

f0b7dc001cb770cad0c0b146cbc5aaed44c289f1
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 12:10:08 +0100

    Submit solution

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*print_triangle - prints a triangle
+*@size:size parameter of triangle
+*Return:returns nothing
+*/
+void print_triangle(int size)
+{
+int inc1, inc2;
+
+if (size > 0)
+{
+for ( inc1 = 1; inc1 <= size; inc1++)
+{
+for ((inc2 = size - inc1); inc2 > 0; inc2--)
+_putchar(' ');
+
+for (inc2 = 0; inc2 < inc1; inc2++)
+_putchar('#');
+
+if (inc1 == size)
+continue;
+
+}
+}
+else
+_putchar('\n');
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,6 +11,7 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
 int _putchar(char);
 int _isupper(int);
 int _isdigit(int);

20fe0eec4ec1bedf81a75c388b01e983eb35615f
Pericles001 <periclesadjovi@outlook.fr>
2021-09-03 13:25:09 +0100

    Add values

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,14 +4,14 @@
 *main- prints the largest prime factor
 *of a number
 *
-*Return: returrns 0
+*Return: returns 0
 */
 
 int main(void)
 {
-long number = 1231952; 
-int inc; 
-while (inc++ < number/2)
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
 {
 if (number % inc == 0)
 {
@@ -19,7 +19,7 @@ number /= 2;
 continue;
 }
 
-for ( inc = 3; inc < number/2; inc += 2)
+for (inc = 3; inc < number / 2; inc += 2)
 {
 if (number % inc == 0)
 number /= inc;

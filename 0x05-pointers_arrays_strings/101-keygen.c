1f27c350d6a7d012e55e1959cf158c038f7aa61d
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 17:35:18 +0100

    Add solution

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point generates a random password
+ *
+ * Return: the generated password
+ */
+int main(void)
+{
+char *password;
+int i;
+srand(times(0));
+while (i <= 2465)
+{
+password = rand() % 128;
+i += password;
+_putchar(password);
+}
+_putchar(2772 - i);
+return (0);
+}

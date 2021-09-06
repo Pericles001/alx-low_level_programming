13dcb2abd71648c4cbbc17c67399388d2e7df2b2
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 12:46:50 +0100

    Solve error and remove trailing space

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
 *reset_to_98 - reset value of an integer to 98
-*
+*@n: parameter targeted
 *Return : nothing
 */
 
 void reset_to_98(int *n)
 {
-return (*n = 98); 
+*n = 98;
 }

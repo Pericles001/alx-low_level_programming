d2d787c45ec5e55789deb96ecbb7481c392586fe
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 16:46:06 +0100

    Add betty style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,11 @@
 char *_strcpy(char *dest, char *src)
 {
 int inc = 0;
-while (*(src + inc ) != '\0')
+while (*(src + inc) != '\0')
 {
-*(dest + inc ) = *(src + inc );
+*(dest + inc) = *(src + inc);
 inc++;
 }
-*(dest + inc ) = '\0';
-return (dest); 
+*(dest + inc) = '\0';
+return (dest);
 }

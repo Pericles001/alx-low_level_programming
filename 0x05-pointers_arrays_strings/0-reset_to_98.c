78756e079f72bfde827b0d333594b2d941d629e1
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 12:42:36 +0100

    Submit solution and add prototype

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*reset_to_98 - reset value of an integer to 98
+*
+*Return : nothing
+*/
+
+void reset_to_98(int *n)
+{
+return (*n = 98); 
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+* void prototypes()
+* int prototypes()
+*/
+void reset_to_98(int *n);
+int _putchar(char c);
+#endif /*MAIN_H*/

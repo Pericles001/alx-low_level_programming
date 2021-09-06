070e3fe36b08f87af5085a056f702c8e37064e2c
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 16:21:05 +0100

    Submit solution

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_array - print n element of array
+*@a: array
+*@n: number of elements
+*Return: nothing
+*/ 
+void print_array(int *a, int n)
+{
+int inc ; 
+for (inc = 0; inc < n ; inc++)
+{
+if (inc != n - 1)
+printf("%d, ", a[inc]);
+else
+printf("%d", a[inc]);
+
+}
+printf("\n");
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 /**
 * void prototypes()
 * int prototypes()
@@ -12,6 +13,7 @@ void swap_int(int *a, int *b);
 void swap_char(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 int _putchar(char c);
 int _strlen(char *s);
 #endif /*MAIN_H*/

de445d3c6562e91f042d3a9aa5b12ed252fe50e1
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 12:57:25 +0100

    Review code

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- *swap_int - swap two integers
- *@a: first integer
- *@b: second integer
- *Return: nothing
- */
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
+*/
 
 void swap_int(int *a, int *b)
 {
-int tmp;
-tmp = *a;
+int tmp = *a;
 *a = *b;
 *b = tmp;
 }

1df8238bda85dc21b20c99c165002d3658f41065
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 13:53:58 +0100

    Submit next solution

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_rev - prints a string in reverse
+* @s: string to print
+* /
+void print_rev(char *s)
+{
+int len = 0, index;
+while (*s)
+len++;
+
+for(index = len - 1; index >= 0; index--)
+_putchar(s[index]);
+
+_putchar('\n');
+}

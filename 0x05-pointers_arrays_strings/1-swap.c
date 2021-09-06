ee5a609aacf74d2c779abad2a36cfd3e4f05c47e
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 12:53:51 +0100

    Add swap prototype and resolve task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *swap_int - swap two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@
 * int prototypes()
 */
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _putchar(char c);
 #endif /*MAIN_H*/

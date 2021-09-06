68f2bafd9f38c5cabd30c3a14cf43ecf818d4882
Pericles001 <periclesadjovi@outlook.fr>
2021-09-06 17:18:42 +0100

    Review code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,12 +10,11 @@ int i = 1;
 unsigned int num = 0;
 do {
 if (*s == '-')
-i = -1;
+i *= -1;
 else if (*s >= '0' && *s <= '9')
 num = num * 10 + (*s - '0');
 else if (num > 0)
 break;
-}
-while (*s++);
+}while (*s++);
 return (num *i);
 }

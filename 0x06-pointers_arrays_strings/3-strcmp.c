cbd2895731136735b1439793be14cbdb6a654928
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 00:14:29 +0100

    Submit solution

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*  _strcmp - Function which compare two strings and
+*@s1: first string
+*@s2:second string
+*Return:
+*		returns zero if s1 == s2
+*		returns negative number if s1 < s2
+*		returns positive number if s1 > s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,8 +5,10 @@
  *int prototypes()
  *char prototypes()
  */  
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
+	int _strlen(char *);
+	int _strcmp(char *s1, char *s2);
+	char *_strcat(char *dest, char *src);
+	char *_strncat(char *dest, char *src, int n);
+	char *_strncpy(char *dest, char *src, int n);
 
 #endif /*MAIN_H*/

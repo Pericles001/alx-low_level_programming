acca2a9adc3a34630037821438acc1106da25962
Pericles001 <periclesadjovi@outlook.fr>
2021-09-08 23:39:28 +0100

    Add betty style

--- 0x06-more_pointers_arrays_strings/2-strncpy.c
+++ 0x06-more_pointers_arrays_strings/2-strncpy.c
@@ -18,12 +18,12 @@ char _strncpy(char *dest, char *src, int n)
 	while (src[index++])
 		src_len++;
 
-  for (index = 0; src[index] && index < n; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 
 	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
 
 	return (dest);
-	
+
 }

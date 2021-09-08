ccadca2ef6ad0233788c35903e774c8bcdecac36
Pericles001 <periclesadjovi@outlook.fr>
2021-09-08 19:28:34 +0100

    Add prototype and submit solution

---
+++ 0x06-more_pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
+
+char *strcat(char *dest, char *src)
+{
+
+int index, dest_len = 0 ;
+
+while (dest[index++])
+dest_len++;
+
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
+
+return (dest);
+
+}
---
+++ 0x06-more_pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ */  
+char *_strcat(char *dest, char *src);
+
+#endif /*MAIN_H*/

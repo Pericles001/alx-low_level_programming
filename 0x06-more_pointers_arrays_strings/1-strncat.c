82133c322a3cd9d927e8315ad7385560eb06d9d0
Pericles001 <periclesadjovi@outlook.fr>
2021-09-08 23:38:03 +0100

    Reindent code

--- 0x06-more_pointers_arrays_strings/1-strncat.c
+++ 0x06-more_pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 
-int index = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
-while (dest[index++])
-dest_len++;
+	while (dest[index++])
+		dest_len++;
 
-for (; src[index] && index < n; index++)
-dest[dest_len++] = src[index];
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
-return (dest);
+	return (dest);
 
 }

66405b0656496780662a1a38179483bc449d0805
Pericles001 <periclesadjovi@outlook.fr>
2021-09-08 19:41:08 +0100

    Review code

--- 0x06-more_pointers_arrays_strings/0-strcat.c
+++ 0x06-more_pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *strcat - concatenates  the string pointed to by @src to
+ *_strcat - concatenates  the string pointed to by @src to
  * the end of the string pointed to by @dest
  *@dest: String that will be appended
  *@src: String to be concatenated upon
@@ -9,7 +9,7 @@
  * Return: returns poiner to @dest
  */
 
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 
 int index = 0, dest_len = 0 ;
--- 0x06-more_pointers_arrays_strings/1-strncat.c
+++ 0x06-more_pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * 
- **strncat - concatenate two strings but add inputted number of bytes
+ *_strncat - concatenate two strings but add inputted number of bytes
  *@dest: string to be appended upon
  *@src: string to be completed at end of dest
+ @n:integer parameter to compare index to
  *Return: returns new concatenated string
  */
 
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 int index = 0, dest_len = 0;
 
 while (dest[index++])
-dest_len++; 
+dest_len++;
 
 for (; src[index] && index < n; index++)
 dest[dest_len++] = src[index];

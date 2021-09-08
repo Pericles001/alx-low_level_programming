463b831fa241ac1e895e69e0ff282c1ca091ad97
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 00:15:08 +0100

    Remove unused header

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,6 @@
  *int prototypes()
  *char prototypes()
  */  
-	int _strlen(char *);
 	int _strcmp(char *s1, char *s2);
 	char *_strcat(char *dest, char *src);
 	char *_strncat(char *dest, char *src, int n);

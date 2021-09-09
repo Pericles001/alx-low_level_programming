5e4041e893f53fcce2e911d83f84fb44c443995a
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 18:46:59 +0100

    Change code content

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,30 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, op = 0;
+	int i = 0, diff = 0;
 
-	while (op == 0)
+	while (1)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+	
 	}
-	return (op);
+	return (diff);
 }

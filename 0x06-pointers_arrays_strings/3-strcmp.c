ff5edfff560c9964f5a395bb6e4f2f75a02115a5
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 18:37:17 +0100

    Restart code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,10 +12,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && (*s1 == *s2))
+	int i, op = 0;
+	while (op == 0)
 	{
-		s1++;
-		s2++;
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
 	}
-	return (*s1 - *s2);
+	return (op);
 }

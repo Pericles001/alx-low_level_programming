329d4db24c37dffa832294018a15cf00e95b2f51
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 01:18:09 +0100

    Submit solution

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,11 +9,12 @@
 void reverse_array(int *a, int n)
 {
 	int tmp, index;
+
 	for (index = n - 1; index > n / 2; index--)
 	{
 		tmp = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
 		a[index] = tmp;
 	}
-	
+
 }

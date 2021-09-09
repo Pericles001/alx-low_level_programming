dd313aeadffbf140a7334279980066487ae9577c
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 01:18:35 +0100

    Add prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@
 	char *_strcat(char *dest, char *src);
 	char *_strncat(char *dest, char *src, int n);
 	char *_strncpy(char *dest, char *src, int n);
+	char *string_toupper(char *);
 
 #endif /*MAIN_H*/

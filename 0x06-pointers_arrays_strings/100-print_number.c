f8de3926d6866193ea6b6334933eb0042de22924
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 02:44:10 +0100

    Submit solution

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_number - prints an integer
+*@n:integer to be printed
+*
+*/
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}

409c0b2ebeae0059ee5ebdccc0f92a9d4b27334c
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 03:09:36 +0100

    Configure file

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* infinite_add - C function that adds two numbers stored
+*in strings to a buffer.
+*Assumes that strings are never empty and
+*that numbers will always be positive, or 0.
+*Assumes there are only digits stored in the number strings.
+*If result can be stored in the buffer,
+*returns a pointer to the result.
+*If result cannot be stored in the buffer, returns `0`.
+*@n1:first number to be added
+*@n2:second number to be added
+*@r: store result
+*@size_r: size of buffer
+*Return:returns pointer to result
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}

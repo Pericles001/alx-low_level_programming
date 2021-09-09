c0ab285cdab769199411032a0efb6bd6c9f398de
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 01:09:57 +0100

    Submit solution

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*  reverse_array - function that reverse content of array
+*  @a: array
+*  @n: number of elements
+*/
+
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+	for (index = n - 1; index > n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+	
+}

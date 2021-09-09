9baccfb15ee85a42bcc8bf69574cf8c22f15193d
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 04:11:36 +0100

    Change indentation style

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,12 +7,14 @@
 */
 int main(void)
 {
-char school[10] = "_putchar";
-int i;
-for (i = 0; i < 8; i++)
-{
-_putchar(school[i]);
-}
-_putchar(10);
-return (0);
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
 }

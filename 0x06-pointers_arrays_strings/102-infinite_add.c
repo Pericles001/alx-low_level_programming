de5269b57c96d2b32c02c3279f6860860a30a226
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 03:56:38 +0100

    Start code for solution

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -18,5 +18,43 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+}
 
+/**
+* add_strings - Adds the numbers stored in two strings.
+* @n1: The string containing the first number to be added.
+* @n2: The string containing the second number to be added.
+* @r: The buffer to store the result.
+* @r_index: The current index of the buffer.
+*
+* Return: If r can store the sum - a pointer to the result.
+*         If r cannot store the sum - 0.
+*/
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--; r_index++)
+	{
+		num = *(n1 - '0') + tens; 
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--;  r_index--)
+	{
+		num = (*n2 - '0') + tens; 
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10; 
+	}
+	
 }

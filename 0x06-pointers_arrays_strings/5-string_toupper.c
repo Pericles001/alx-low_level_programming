e3f9014d445004e0044a7c3f2c4b799d72ae4980
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 01:18:21 +0100

    Submit solution

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+*string_toupper - changes all lowercases letters of a string to uppercase
+*@str: parameter
+*Return: returns a character
+*/
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index++])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+		
+	}
+
+	return (str);
+}

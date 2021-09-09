30e6eafa1475942b9bffb4963ac7d49ded386954
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 03:09:51 +0100

    Configure file

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_buffer -  C function that prints the content of an
+*  inputted number of bytes from a buffer.
+* Prints 10 bytes per line.
+* Starts with the position of the first byte in hexadecimal (8 chars),
+* starting with `0`.
+* Each line shows the hexadecimal content (2 chars) of the buffer,
+* 2 bytes at a time, separated by a space.
+* Each line shows the content of the buffer.
+* Prints the byte if it is printable; if not, prints `.`.
+* Each line ends with a new line `\n`.
+* If the inputted byte size is 0 or less, the function only prints a new line.
+*@b: number of bytes
+*@size: size of the byte
+*/
+void print_buffer(char *b, int size)
+{
+	
+}

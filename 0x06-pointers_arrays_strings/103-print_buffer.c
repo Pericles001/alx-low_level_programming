ed104f53a9468ab117b02c8e5de2df72fb94bc3c
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 03:31:44 +0100

    Submit solution

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -16,5 +16,36 @@
 */
 void print_buffer(char *b, int size)
 {
-	
+	int i = 0, j;
+	if (size < 0)
+	{
+		printf('\n');
+		return;
+	}
+
+	while (i < size)
+	{
+		if (i % 10 == 0)
+			printf("%08x: ", i);
+		for (j = i; j < i + 9; j += 2)
+		{
+			if ((j < size) && ((j + 1) < size))
+				printf("%02x%02x: ", b[j], b[j + 1]);
+			else
+			{
+				while (++j <= i + 10)
+					printf(" ");
+				printf(" ");
+			}
+		}
+		for (j = i; j < i + 9 && j < size; j++)
+		{
+			if (b[j] >= 32 && b[j] <= 126)
+				printf("%c", b[j]);
+			else
+				printf(".");
+		}
+		printf('\n');
+		i += 10;
+	}
 }

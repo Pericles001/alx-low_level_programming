6d168f6c2bad6bbb62339fa66647e958e8cd370e
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 03:09:15 +0100

    Add new library

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 /**
  *void prototypes()
  *int prototypes()
@@ -7,6 +8,7 @@
  */  
 	void reverse_array(int *a, int n);
 	void print_number(int n);
+	void print_buffer(char *b, int size)
 	int _strcmp(char *s1, char *s2);
 	char *_strcat(char *dest, char *src);
 	char *_strncat(char *dest, char *src, int n);
@@ -15,5 +17,6 @@
 	char *cap_string(char *);
 	char *leet(char *);
 	char *rot13(char *);
+	char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /*MAIN_H*/

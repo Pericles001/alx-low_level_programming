34b35831488bd760c8e0ca8e1bcb803fdf5da7b3
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 02:43:57 +0100

    Add prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
  *char prototypes()
  */  
 	void reverse_array(int *a, int n);
+	void print_number(int n);
 	int _strcmp(char *s1, char *s2);
 	char *_strcat(char *dest, char *src);
 	char *_strncat(char *dest, char *src, int n);

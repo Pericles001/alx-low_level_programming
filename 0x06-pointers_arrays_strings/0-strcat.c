b858da44fd0209dd7450a226a29e8dce4835247e
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 13:15:28 +0100

    Change indentation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,14 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int index = 0, dest_len = 0;
+int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+while (dest[index++])
+dest_len++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 
-	return (dest);
-
+return (dest);
 }

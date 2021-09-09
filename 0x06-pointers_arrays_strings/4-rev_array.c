268526c2e2ff45de5cce52d791fcaf7077b39cc8
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 18:51:49 +0100

    Change code content

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,11 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
-
-	for (index = n - 1; index > n / 2; index--)
-	{
-		tmp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = tmp;
-	}
-
+int tmp, index;
+for (index = n - 1; index > n / 2; index--)
+{
+tmp = a[n - 1 - index];
+a[n - 1 - index] = a[index];
+a[index] = tmp;
+}
 }

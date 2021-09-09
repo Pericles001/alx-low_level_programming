bfd1c9a29eceef3c3cb2f51aac3effc35b750269
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 13:20:06 +0100

    Repair main.h and reindent file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,8 @@
  */
 void reverse_array(int *a, int n);
 void print_number(int n);
-void print_buffer(char *b, int size) int _strcmp(char *s1, char *s2);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);

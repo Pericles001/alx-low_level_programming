1fd169403a6686b6c10701011b34a1ed8d1a8e1d
Pericles001 <periclesadjovi@outlook.fr>
2021-09-09 03:56:23 +0100

    Add prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,18 +5,18 @@
  *void prototypes()
  *int prototypes()
  *char prototypes()
- */  
-	void reverse_array(int *a, int n);
-	void print_number(int n);
-	void print_buffer(char *b, int size)
-	int _strcmp(char *s1, char *s2);
-	char *_strcat(char *dest, char *src);
-	char *_strncat(char *dest, char *src, int n);
-	char *_strncpy(char *dest, char *src, int n);
-	char *string_toupper(char *);
-	char *cap_string(char *);
-	char *leet(char *);
-	char *rot13(char *);
-	char *infinite_add(char *n1, char *n2, char *r, int size_r);
+ */
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size) int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /*MAIN_H*/

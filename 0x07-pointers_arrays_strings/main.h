212170959ac31951a7295ec6055eb4eac0ca648b
Pericles001 <periclesadjovi@outlook.fr>
2021-09-14 18:35:45 +0100

    Add prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
-
+char *_strstr(char *haystack, char *needle);
 #endif /* MAIN_H */

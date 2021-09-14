741b0ead62ed2e68c3dae75edc484ddc38e3a5c4
Pericles001 <periclesadjovi@outlook.fr>
2021-09-14 14:51:46 +0100

    Add betty style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *_memset - The _memset() function fills
-* the first n bytes of the memory area 
+* the first n bytes of the memory area
 * pointed to by s with the constant byte b
 *@s:target
 *@b: constant byte
@@ -12,8 +12,8 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	while (n--)
-		s[n-1] = b;
+		s[n - 1] = b;
 
 	return (s);
-	
+
 }

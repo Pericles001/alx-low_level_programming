34d996b8242d6c0be11a9c3afc3723de383369ea
Pericles001 <periclesadjovi@outlook.fr>
2021-09-14 03:05:50 +0100

    Add prototype

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char)
+*/
+ char *_memset(char *s, char b, unsigned int n);
+
+
+#endif /* MAIN_H */

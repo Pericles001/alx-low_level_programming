6f0402867a6e3b8541ec16056ba67dfe41a326cf
Pericles001 <periclesadjovi@outlook.fr>
2021-09-14 18:00:25 +0100

    Add prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
 
 #endif /* MAIN_H */

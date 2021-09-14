84a3d5aa3765d3b3191e60adda1461e353fa3554
Pericles001 <periclesadjovi@outlook.fr>
2021-09-14 21:37:38 +0100

    Add prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@
 */
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 unsigned int _strspn(char *s, char *accept);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);

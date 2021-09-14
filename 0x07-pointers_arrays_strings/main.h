8a004df04d62481bc5ad5014252349de5b3c2d15
Pericles001 <periclesadjovi@outlook.fr>
2021-09-14 17:10:21 +0100

    Add prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 /**
 *void prototypes(void)
 *int prototypes(int)
 *char prototypes(char)
 */
+unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);

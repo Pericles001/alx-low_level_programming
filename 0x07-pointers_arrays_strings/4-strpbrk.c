7f285304edcd070a6c95ceed1f503d15a152d3d0
Pericles001 <periclesadjovi@outlook.fr>
2021-09-14 20:44:24 +0100

    Submit solution

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,6 +6,9 @@
 *
 *@s: string where search is made
 *@accept: string where searched bytes are located
+*
+*Return:Returns a pointer to the byte in s that matches
+* one of the bytes in accept, or NULL if no such byte is found
 */
 
 char *_strpbrk(char *s, char *accept)

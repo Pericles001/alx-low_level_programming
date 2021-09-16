79c10cdb37c8f3317ed3743e6b36a58d9fcb8bed
Pericles001 <periclesadjovi@outlook.fr>
2021-09-16 19:26:54 +0100

    Add prototypes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,12 @@
 void _puts_recursion(char *s);
  void _print_rev_recursion(char *s);
 int _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */

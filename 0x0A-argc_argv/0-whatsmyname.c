b5f6896e08d79625ae37beea158fcba82587f355
Pericles001 <periclesadjovi@outlook.fr>
2021-09-18 00:09:22 +0100

    Review code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 
 /**
 * main - prints program name
-*@argc: number of arguments 
+*@argc: number of arguments
 *@argv: array of arguments
-*Returns: 0
+*Return: 0
 */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (0)
+	return (0);
 }

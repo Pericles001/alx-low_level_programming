a683ee46d4c8698353881cb8970f2129947a0abf
Pericles001 <periclesadjovi@outlook.fr>
2021-09-17 23:47:23 +0100

    Create file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints program name
+*@argc: number of arguments 
+*@argv: array of arguments
+*Returns: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0)
+}

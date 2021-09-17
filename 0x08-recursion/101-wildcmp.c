b5325a8749ca0af2bbf29c78a2d49c62c527e48d
Pericles001 <periclesadjovi@outlook.fr>
2021-09-17 20:57:20 +0100

    Review code

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,6 +13,9 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '\0')
 		return (*s1 == '\0');
 	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1) || (*s1 == '\0' && wildcmp(s1 + 1, s2)));
+
 	return (0);
 }

5208c51c837499d4f7cd7f5b31d8687219055f5f
Pericles001 <periclesadjovi@outlook.fr>
2021-09-17 01:13:36 +0100

    Review code

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,14 +3,14 @@
 * is_prime_number - returns true if the number is prime
 *@n: the number to check
 *
-*Returns true if the number is prime
+*Return: true if the number is prime
 */
 int is_prime_number(int n)
 {
 	int start = n / 2;
 
 	if (n <= 1)
-		return 0;
+		return (0);
 	return (is_prime(n, start));
 }
 
@@ -28,5 +28,5 @@ int is_prime(int n, int start)
 		return (1);
 	else if (n % start == 0)
 		return (0);
-	return is_prime(n, start - 1);
+	return (is_prime(n, start - 1));
 }

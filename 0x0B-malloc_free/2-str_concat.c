09b3e3a2f7984f9402e2ce2cb76fa13c72a2097a
Pericles001 <periclesadjovi@outlook.fr>
2021-09-21 20:03:12 +0100

    Review code

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 		concat_str[concat_index++] = s1[index];
 
 	for (index = 0; s2[index]; index++)
-		concat_str[concat_index] = s2[index];
+		concat_str[concat_index++] = s2[index];
 
 	return (concat_str);
 }

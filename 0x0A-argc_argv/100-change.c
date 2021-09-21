55d2400f05bd5527d4671719c0edba510ce12d73
Pericles001 <periclesadjovi@outlook.fr>
2021-09-21 10:16:50 +0100

    Create file

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - prints the minimum number of coins to make change for an amount of money
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 1 if there is an error; else returns 0
+*/
+
+int main()
+{
+
+
+}

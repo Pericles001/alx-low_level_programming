4aa316758a44211015a1f4a63d6cc2b090cac8cc
Pericles001 <periclesadjovi@outlook.fr>
2021-09-21 17:45:51 +0100

    Submit solution

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,11 +5,24 @@
 * initializes it with a specific char.
 *@size: Size of the array
 *@c: Character to insert
-*Return: NULL if size is zero or if it fails , 
-*pointer to array if everything is normal. 
+*Return: NULL if size is zero or if it fails,
+*pointer to array if everything is normal.
 */
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int index;
 
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
 }

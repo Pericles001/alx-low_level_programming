274b171c1344f2d201a2f702711319e0607386ad
Pericles001 <periclesadjovi@outlook.fr>
2021-09-21 17:26:00 +0100

    Start first task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* create_array - creates an array of chars, and
+* initializes it with a specific char.
+*@size: Size of the array
+*@c: Character to insert
+*Return: NULL if size is zero or if it fails , 
+*pointer to array if everything is normal. 
+*/
+
+char *create_array(unsigned int size, char c)
+{
+
+}

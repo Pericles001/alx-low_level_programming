e12c4f3162f70ac7f3422929050505a95d40589d
Pericles001 <periclesadjovi@outlook.fr>
2021-09-23 22:41:40 +0100

    Add len prototype

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,6 +7,8 @@ void free_grid(int **grid, int height);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 int **alloc_grid(int width, int height);
-
+int len(char *str);
 #endif /* MAIN_H */

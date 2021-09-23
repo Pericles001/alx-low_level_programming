de558de13d0dceb422bc6b3d6e129cda2f8683cc
Pericles001 <periclesadjovi@outlook.fr>
2021-09-23 12:07:02 +0100

    Add new prototype

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,8 +3,10 @@
 
 #include <stdlib.h>
 
+void free_grid(int **grid, int height);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif /* MAIN_H */

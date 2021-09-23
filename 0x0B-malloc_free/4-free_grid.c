0e57836a374e5b590724573f795af8ae2f5928f7
Pericles001 <periclesadjovi@outlook.fr>
2021-09-23 14:07:38 +0100

    Submit solution

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* free_grid - frees a 2d array
+*@grid:memory block to be freed
+*@height:height of the array
+*Return:returns void
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid[i]);
+}

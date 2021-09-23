7f6833368f2ffb65b95ca1d2ed07507a7a8d74c5
Pericles001 <periclesadjovi@outlook.fr>
2021-09-23 23:39:11 +0100

    Review code

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,46 +9,46 @@
 
 char **strtow(char *str)
 {
-		char **split;
-		int i, j = 0, temp = 0, size = 0, words = num_words(str);
+char **split;
+int i, j = 0, temp = 0, size = 0, words = num_words(str);
 
-		if (words == 0)
-			return (NULL);
-		split = (char **) malloc(sizeof(char *) * (words + 1));
-		if (split != NULL)
-		{
-			for (i = 0; i <= len(str) && words; i++)
-			{
-				if ((str[i] != ' ') && (str[i] != '\0'))
-					size++;
-				else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-				{
-					split[j] = (char *) malloc(sizeof(char) * size + 1);
-					if (split[j] != NULL)
-					{
-						while (temp < size)
-						{
-							split[j][temp] = str[(i - size) + temp];
-							temp++;
-						}
-						split[j][temp] = '\0';
-						size = temp = 0;
-						j++;
-					}
-					else
-					{
-						while (j-- >= 0)
-							free(split[j]);
-						free(split);
-						return (NULL);
-					}
-				}
-			}
-			split[words] = NULL;
-			return (split);
-		}
-		else
-			return (NULL);
+if (words == 0)
+return (NULL);
+split = (char **) malloc(sizeof(char *) * (words + 1));
+if (split != NULL)
+{
+for (i = 0; i <= len(str) && words; i++)
+{
+if ((str[i] != ' ') && (str[i] != '\0'))
+size++;
+else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+{
+split[j] = (char *) malloc(sizeof(char) * size + 1);
+if (split[j] != NULL)
+{
+while (temp < size)
+{
+split[j][temp] = str[(i - size) +temp];
+temp++;
+}
+split[j][temp] = '\0';
+size = temp = 0;
+j++;
+}
+else
+{
+while (j-- >= 0)
+free(split[j]);
+free(split);
+return (NULL);
+}
+}
+}
+split[words] = NULL;
+return (split);
+}
+else
+return (NULL);
 }
 
 
@@ -60,25 +60,25 @@ char **strtow(char *str)
 */
 int num_words(char *str)
 {
-	int i = 0, words = 0;
+int i = 0, words = 0;
 
-	while (i <= len(str))
-	{
-		if ((str[i] != ' ') && (str[i] != '\0'))
-		{
-			i++;
-		}
-		else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-		{
-			words += 1;
-			i++;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	return (words);
+while (i <= len(str))
+{
+if ((str[i] != ' ') && (str[i] != '\0'))
+{
+i++;
+}
+else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+{
+words += 1;
+i++;
+}
+else
+{
+i++;
+}
+}
+return (words);
 }
 
 /**
@@ -90,12 +90,12 @@ int num_words(char *str)
 
 int len(char *str)
 {
-	int len = 0;
+int len = 0;
 
-	if (str != NULL)
-	{
-		while (str[len])
-			len++;
-	}
-	return (len);
+if (str != NULL)
+{
+while (str[len])
+len++;
+}
+return (len);
 }

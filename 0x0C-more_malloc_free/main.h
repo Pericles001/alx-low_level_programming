bac5fc31702317b60fe44db07332fd7d892d2bab
Pericles001 <periclesadjovi@outlook.fr>
2021-09-24 17:21:09 +0100

    Add prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,8 @@
 #define MAIN_H
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif /* MAIN_H */

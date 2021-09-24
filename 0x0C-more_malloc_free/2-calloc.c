89b2aada63545dfbffc5e23ebdf259479c3f386c
Pericles001 <periclesadjovi@outlook.fr>
2021-09-24 17:09:04 +0100

    Update code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,19 +9,18 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *target;
+	char *block;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	target = malloc(nmemb * size);
-
-	if (target != NULL)
-	{
-		for (i = 0; i < (nmemb * size); i++)
-			target[i] = 0;
-		return (target);
-	}
+  if (nmemb == 0 || size == 0)
+    return (NULL);
+  block = malloc(nmemb * size);
+  if (block != NULL)
+  {
+    for (i = 0; i < (nmemb * size); i++)
+      block[i] = 0;
+    return (block);
+  }
 	else
 		return (NULL);
 }

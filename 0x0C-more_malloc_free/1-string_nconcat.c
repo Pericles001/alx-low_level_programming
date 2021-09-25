8459156f91421a3b71822d4a9a4f17791fa0040a
Pericles001 <periclesadjovi@outlook.fr>
2021-09-25 21:22:16 +0100

    Review code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 
@@ -8,7 +7,6 @@
  *@s1: The first string.
  *@s2: The second string.
  *@n: The maximum number of bytes of s2 to concatenate to s1.
- *
  *Return: If the function fails - NULL.
  *Otherwise - a pointer to the concatenated space in memory.
  */

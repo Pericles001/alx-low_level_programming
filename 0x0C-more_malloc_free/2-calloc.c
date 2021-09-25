50c2ffb5f2eae5b3324d852a2bb2cc70759cf6ce
Pericles001 <periclesadjovi@outlook.fr>
2021-09-25 11:36:03 +0100

    Reindent code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,15 +12,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *block;
 	unsigned int i;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  block = malloc(nmemb * size);
-  if (block != NULL)
-  {
-    for (i = 0; i < (nmemb * size); i++)
-      block[i] = 0;
-    return (block);
-  }
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	block = malloc(nmemb * size);
+	if (block != NULL)
+	{
+		for (i = 0; i < (nmemb * size); i++)
+			block[i] = 0;
+		return (block);
+	}
 	else
 		return (NULL);
 }

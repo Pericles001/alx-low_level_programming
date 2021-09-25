37499d7ca00b12b58142804e66f1651708e6c510
Pericles001 <periclesadjovi@outlook.fr>
2021-09-25 21:18:41 +0100

    Review prototypes

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
 int _putchar(char c);
@@ -12,5 +13,10 @@ int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 
 #endif /*MAIN_H*/

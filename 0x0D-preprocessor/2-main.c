98162a88043023c4076eb984aecf915824d2aca7
Pericles001 <periclesadjovi@outlook.fr>
2021-09-27 13:27:23 +0100

    Revie code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-*main - prints the name of 
+*main - prints the name of
 *the file it was compiled from
-*
+*@argc:number of input arguments
+*@argv:array of outputs
 *Return: nothing to be returned
 */
 
-int main(int __attribute__((unused)) argc, char ** argv)
+int main(int __attribute__((unused)) argc, char **argv)
 {
 printf("%s \n", argv[0]);
 return (0);

a3984b4999ec40b36f7510282e97f5c4b5443263
Pericles001 <periclesadjovi@outlook.fr>
2021-09-27 14:13:26 +0100

    Add struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+#endif

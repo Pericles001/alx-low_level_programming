299bbff6f68f20209c93a8c81975371e55e1fa24
Pericles001 <periclesadjovi@outlook.fr>
2021-09-27 22:20:16 +0100

    Submit solution

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,43 @@
 #include "dog.h"
 
+
+/**
+*_strlen - returns length of
+*a string
+*@str: string to be counted
+*Return: returns length of string
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (str)
+len++;
+
+return (len);
+}
+
+
+/**
+*_strcopy - copy string pointed by src
+*into dest variable
+*@dest:buffer storing string copy
+*@src: buffer storing string to copy
+*Return:returns copied string
+*/
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+
+for (; src[index] ; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
+
+
+
+
 /**
 *new_dog - creates a new dog
 *@name: name of new dog
@@ -11,5 +49,34 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t *dog;
 
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+
+dog = malloc(sizeof(dog_t));
+
+if (dog == NULL)
+return (NULL);
+
+dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+
+dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+
+dog->name = _strcopy(dog->name = name);
+dog->age = age;
+dog->owner = _strcopy(dog->owner = owner);
+
+return (dog);
 }

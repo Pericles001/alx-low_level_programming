d9f9d2110d6394a097d1f26462c6691a6ea20d4e
Pericles001 <periclesadjovi@outlook.fr>
2021-09-27 22:22:09 +0100

    Submit solution

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -29,4 +29,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
 #endif

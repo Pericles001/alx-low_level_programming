9c42085416eca21181fb4fd0e9f58fe44013ffee
pericles001 <periclesadjovi@outlook.fr>
2021-09-29 20:29:02 +0100

    Start solution

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+*int_index - searches for an integer
+*@array: Array containing elements
+*@size: number of elements in array
+*@cmp: function which compare values
+*
+*Return: returns -1 if no element match
+* or when size is less than zero
+*return pointer to the first corresponding element
+*
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+}

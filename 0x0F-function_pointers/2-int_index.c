265e6feca281df0a99d97e6d4b7a2b7904dbd380
pericles001 <periclesadjovi@outlook.fr>
2021-09-29 20:47:31 +0100

    Submit solution

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,5 +14,17 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+int index = 0;
+
+if (array == NULL || cmp == NULL)
+return (-1);
+
+for (; index < size; index++)
+{
+if (cmp(array[index]) != 0)
+return (index);
+}
+
+return (-1);
 
 }

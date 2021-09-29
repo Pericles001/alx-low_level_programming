ce38545e369aaee7c67fca5881a51266cbafd903
pericles001 <periclesadjovi@outlook.fr>
2021-09-29 20:08:03 +0100

    Submit solution

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_name - prints a name
+*@name:name to print
+*@f: function which print name
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+
+if (name == NULL || f == NULL)
+return;
+
+f(name);
+
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*array_iterator - executes a function on each element of an array
+*@array: array to be targeted
+*@size: size of array
+*@action: function to be executed
+*
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+if (array == NULL || action == NULL)
+return;
+
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
+
+}

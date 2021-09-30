5055f1dbcf97e5996aaf3ff1c87af6c48ef59091
pericles001 <periclesadjovi@outlook.fr>
2021-10-01 00:37:38 +0100

    Review code

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,13 +1,5 @@
 #include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
-
-
 /**
 *op_add - Returns the sum of two numbers
 *@a:the first number

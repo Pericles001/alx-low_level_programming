2c9f4b1ac8857c82b7c232e7808e0a9c545333dd
pericles001 <periclesadjovi@outlook.fr>
2021-10-01 13:42:02 +0100

    Review code

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,16 @@ int sum_them_all(const unsigned int n, ...)
 {
 
 va_list li;
-int sum = 0, i;
+int sum = 0;
+unsigned int i;
 
 va_start(n, li);
 
+if (n != 0)
+{
 for (i = 0; i < n; i++)
 sum += va_arg(li, int);
+}
 
 va_end(li);
 return (sum);

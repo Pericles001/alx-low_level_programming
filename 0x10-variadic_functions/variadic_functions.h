c7fdc898a4cea1382445c0fdf41de85ef3df246b
pericles001 <periclesadjovi@outlook.fr>
2021-10-02 11:09:55 +0100

    Change header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,7 +7,6 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);

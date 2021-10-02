fe9e475afddf06309ea30c05094eda2244ce5ae8
pericles001 <periclesadjovi@outlook.fr>
2021-10-02 11:20:01 +0100

    Add structure definition

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+typedef struct printer{
+char *symbol;
+void (*print)(va_list arg);
+} printer_t;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

9ec84ee4f9cd3bc4cabd4437b46b9e1f19f63679
pericles001 <periclesadjovi@outlook.fr>
2021-10-02 11:03:19 +0100

    Submit solution

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,7 +8,11 @@
 */
 void print_char(va_list arg)
 {
+char letter;
 
+letter = va_arg(arg, int);
+
+printf("%c", letter);
 }
 
 /**
@@ -18,7 +22,11 @@ void print_char(va_list arg)
 */
 void print_int(va_lis arg)
 {
+int num;
 
+num = va_arg(num, int);
+
+printf("%d", num);
 }
 
 
@@ -29,7 +37,11 @@ void print_int(va_lis arg)
 */
 void print_float(va_list arg)
 {
+float num;
 
+num = va_arg(arg, double);
+
+printf("%f", num);
 }
 
 
@@ -39,9 +51,19 @@ void print_float(va_list arg)
 * @arg: A list of arguments pointing to
 * the string to be printed.
 */
-void print_float(va_list arg)
+void print_string(va_list arg)
 {
+char *str;
 
+str = va_arg(arg, char *);
+
+if (str == NULL)
+{
+printf("(nil)");
+return;
+}
+
+printf("%s", str);
 }
 
 /**
@@ -52,5 +74,40 @@ void print_float(va_list arg)
 */
 void print_all(const char * const format, ...)
 {
+va_list args;
 
+int i = 0, j = 0;
+
+char *separator = "";
+
+printer_t funcs[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_string}
+};
+
+va_start(args, format);
+
+while (format && (*(format + i)))
+{
+j = 0;
+
+while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+j++;
+
+if (j < 4)
+{
+printf("%s", separator);
+funcs[j].print(args);
+separator = ", ";
+}
+
+i++;
+
+}
+
+printf("\n");
+
+va_end(args);
 }

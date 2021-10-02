1ed21fb63f17e87a06e12a5f777533c17ec3f753
pericles001 <periclesadjovi@outlook.fr>
2021-10-02 02:32:42 +0100

    Add functions description

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,49 @@
 #include "variadic_functions.h"
 
+
+/**
+* print_char - Prints a char.
+* @arg: A list of arguments pointing to
+* the character to be printed.
+*/
+void print_char(va_list arg)
+{
+
+}
+
+/**
+* print_int - Prints an int.
+* @arg: A list of arguments pointing to
+* the integer to be printed.
+*/
+void print_int(va_lis arg)
+{
+
+}
+
+
+/**
+* print_float - Prints a float.
+* @arg: A list of arguments pointing to
+* the float to be printed.
+*/
+void print_float(va_list arg)
+{
+
+}
+
+
+
+/**
+* print_string - Prints a string.
+* @arg: A list of arguments pointing to
+* the string to be printed.
+*/
+void print_float(va_list arg)
+{
+
+}
+
 /**
 *print_all - prints anything
 *@format: format of input

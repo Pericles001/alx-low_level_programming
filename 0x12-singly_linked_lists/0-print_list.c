c5db8f272917d4a3d8cde8bf5ad4318c2e84d30f
pericles001 <periclesadjovi@outlook.fr>
2021-10-08 13:26:40 +0100

    Remove indentation

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t *cursor = h;
-	size_t count = 0;
+const list_t *cursor = h;
+size_t count = 0;
 
-	while (cursor != NULL)
-	{
-		if (cursor->str != NULL)
-			printf("[%d] %s\n", cursor->len, cursor->str);
-		else
-			printf("[0] (nil)\n");
-		count += 1;
-		cursor = cursor->next;
-	}
+while (cursor != NULL)
+{
+if (cursor->str != NULL)
+printf("[%d] %s\n", cursor->len, cursor->str);
+else
+printf("[0] (nil)\n");
+count += 1;
+cursor = cursor->next;
+}
 
-	return (count);
+return (count);
 }
 

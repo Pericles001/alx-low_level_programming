8e3b706b9810a48d4efd824efc0e31c642a4fb0b
pericles001 <periclesadjovi@outlook.fr>
2021-10-08 13:32:56 +0100

    Remove spaces

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);

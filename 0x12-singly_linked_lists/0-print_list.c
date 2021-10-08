b691f640b0130aed210cc6f2bfeba26848573ad9
pericles001 <periclesadjovi@outlook.fr>
2021-10-08 02:46:37 +0100

    Code review

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,14 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
 		nodes++;
 		h = h->next;
-}
+	}
 
-return (nodes);
+	return (nodes);
 }
 

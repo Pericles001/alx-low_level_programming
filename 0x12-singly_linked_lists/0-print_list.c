0160bea3282e6a4443c9796f4672ee6476ad5276
pericles001 <periclesadjovi@outlook.fr>
2021-10-08 02:45:03 +0100

    Change style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,17 @@
 
 size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
+	size_t nodes = 0;
 
-while (h)
-{
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-nodes++;
-h = h->next;
+		nodes++;
+		h = h->next;
 }
 
 return (nodes);

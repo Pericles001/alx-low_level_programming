b03bbea11d7bbfc09cc1be78ebfd34268d0955a8
pericles001 <periclesadjovi@outlook.fr>
2021-10-12 13:38:27 +0100

    Remove indentation

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,14 +11,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+size_t nodes = 0;
 
-	while (h)
-	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-	return (nodes);
+while (h)
+{
+nodes++;
+printf("%d\n", h->n);
+h = h->next;
 }
-
+return (nodes);
+}

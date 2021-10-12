1300c1c2f51085693397302837aa8584576261cc
pericles001 <periclesadjovi@outlook.fr>
2021-10-12 13:55:29 +0100

    Change code base

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
+	size_t nodes = 0;
 
-while (h)
-{
-nodes++;
-printf("%d\n", h->n);
-h = h->next;
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
 }
-return (nodes);
-}
+

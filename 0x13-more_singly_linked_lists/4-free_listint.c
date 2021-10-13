e93a520bffcb45c64637cc561492fca710745856
pericles001 <periclesadjovi@outlook.fr>
2021-10-13 02:39:04 +0100

    Submit solution

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+*free_listint - frees a linked list
+*@head: head of the list
+*
+*Return: void
+*/
+
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+
+while(head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}

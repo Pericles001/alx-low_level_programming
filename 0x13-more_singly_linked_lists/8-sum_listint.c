c17b95111775695cc02533463245dce5df0afa5c
pericles001 <periclesadjovi@outlook.fr>
2021-10-13 19:12:15 +0100

    Submit solution

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *sum_listint - sums up all the data in a linked list
+ *@head: head of the list
+ *
+ *Return: sum of the number
+ */
+
+int sum_listint(listint_t *head)
+{
+listint_t sum = 0;
+
+while (cursor != NULL)
+{
+sum += cursor->n;
+cursor = cursor->next;
+}
+return (sum);
+}

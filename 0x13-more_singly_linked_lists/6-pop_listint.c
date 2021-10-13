e5aa2ed43c3d4571b5c841e1201416e2854a585d
pericles001 <periclesadjovi@outlook.fr>
2021-10-13 03:15:50 +0100

    Submit solution

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*pop_listint - deletes the head node of a linked list
+*@head: head of the list
+*
+*Return: the deleted node data
+*/
+
+int pop_listint(listint_t **head)
+{
+listint_t *popped;
+int content;
+
+if (*head == NULL)
+return (0);
+popped = *head;
+content = popped->n;
+free(popped);
+
+*head = (*head)->next;
+return (content);
+
+}

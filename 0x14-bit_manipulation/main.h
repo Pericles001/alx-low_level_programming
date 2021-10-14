e898fa6f588f5f0f53ff6b3126cfef51b6c4c377
pericles001 <periclesadjovi@outlook.fr>
2021-10-14 22:52:56 +0100

    Add stdlib

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);

03c41c465a0e08adae393ab6c8368141eaf4bb03
pericles001 <periclesadjovi@outlook.fr>
2021-10-14 23:46:51 +0100

    Include prototype

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
+int check_valid_string(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);

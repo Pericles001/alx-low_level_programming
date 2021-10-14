6e2270a8a90096090955e7bf1c74d4212746be1b
pericles001 <periclesadjovi@outlook.fr>
2021-10-15 00:43:06 +0100

    Reindent code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int str_len = 0, base = 1;
+unsigned int decimal = 0;
+int str_len = 0, base = 1;
 
-	if (!check_valid_string(b))
-		return (0);
-	while (b[str_len] != '\0')
-		str_len++;
-	while (str_len)
-	{
-		decimal += ((b[str_len - 1] - '0') * base);
-		base *= 2;
-		str_len--;
-	}
-	return (decimal);
+if (!check_valid_string(b))
+return (0);
+while (b[str_len] != '\0')
+str_len++;
+while (str_len)
+{
+decimal += ((b[str_len - 1] - '0') * base);
+base *= 2;
+str_len--;
+}
+return (decimal);
 }
 
 /**
@@ -30,13 +30,13 @@ unsigned int binary_to_uint(const char *b)
  */
 int check_valid_string(const char *b)
 {
-	if (b == NULL)
-		return (0);
-	while (*b)
-	{
-		if (*b != '1' && *b != '0')
-			return (0);
-		b++;
-	}
-	return (1);
+if (b == NULL)
+return (0);
+while (*b)
+{
+if (*b != '1' && *b != '0')
+return (0);
+b++;
+}
+return (1);
 }

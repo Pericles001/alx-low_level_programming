ece9e69d2133b545e46ebc9afe733c764065407f
pericles001 <periclesadjovi@outlook.fr>
2021-10-16 00:59:10 +0100

    Submit solution

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*set_bit - sets a bit at position to 1
+*@n: number to be used
+*@index: index to be set
+*
+*Return: 1 on success, -1 on failure
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || (index > sizeof(unsigned long int) * 8) - 1)
+		return (-1);
+	
+	*n |= (1 << index);
+	return (1);
+}

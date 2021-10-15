90b0c83a05122fdee02bcef944a77fce1e5947ba
pericles001 <periclesadjovi@outlook.fr>
2021-10-15 23:48:52 +0100

    Add _putchar

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
 
 unsigned int binary_to_uint(const char *b);
 int check_valid_string(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);

9cc7c10aad4724c4630585240cbb177af7185fe1
pericles001 <periclesadjovi@outlook.fr>
2021-10-15 23:47:24 +0100

    Submit solution

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,16 @@
 #include "main.c"
 
 /**
-*print_binary - 
+*print_binary - prints the binary representation of a number
+*@n: number to be printed
 *
-*
+*Return: void
 */
+
+void print_binary(unsigned long int n)
+{
+if (n > 1)
+print_binary(n >> 1);
+
+_putchar((n & 1) + '0');
+}

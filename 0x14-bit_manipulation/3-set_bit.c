edd9539f7726510f845207af154790a1ca80fe14
pericles001 <periclesadjovi@outlook.fr>
2021-10-16 01:26:01 +0100

    Remove trailing space

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (n == NULL || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
-	
+
 	*n |= (1 << index);
 	return (1);
 }

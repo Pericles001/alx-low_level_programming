986a6a75e28c2110fb1d58ef055153ea13db07a2
pericles001 <periclesadjovi@outlook.fr>
2021-10-21 01:19:42 +0100

    Delete file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);

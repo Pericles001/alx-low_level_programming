4d11e808cc5d3fad716c6f1dd92dc6755cd3c134
pericles001 <periclesadjovi@outlook.fr>
2021-10-23 14:52:20 +0100

    Review code and add ; to line 22

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
 	}
 

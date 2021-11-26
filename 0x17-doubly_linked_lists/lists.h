d9776077c212200c648b1417bd261ac381d68fa5
pericles001 <periclesadjovi@outlook.fr>
2021-11-26 21:59:41 +0100

    Rename file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -23,4 +23,5 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 #endif 

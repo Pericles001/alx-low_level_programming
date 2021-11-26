1e652dedf086367ca1b978502bd4e815750db249
pericles001 <periclesadjovi@outlook.fr>
2021-11-26 20:51:00 +0100

    Rename file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: pointer to the head of the list
- * 
+ *
  * Return: number of nodes
  */
 
@@ -14,10 +14,10 @@ size_t print_dlistint(const dlistint_t *h)
 
 	if (h == NULL)
 		return (counter);
-	
+
 	while (h->prev != NULL)
 		h = h->prev;
-	
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif 

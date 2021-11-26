e4ec18a87bed3a6ca2da59a2019de3a4b02e2c94
pericles001 <periclesadjovi@outlook.fr>
2021-11-26 20:27:44 +0100

    Remove trailing spaces

---
+++ 0x17-doubly_linked_lists/0-print_distint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to the head of the list
+ * 
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	int counter = 0;
+
+	if (h == NULL)
+		return (counter);
+	
+	while (h->prev != NULL)
+		h = h->prev;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
+}

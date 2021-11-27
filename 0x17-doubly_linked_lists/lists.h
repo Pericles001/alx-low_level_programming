e63c5c2bedf3781178258b4dcbb8f9593669643a
pericles001 <periclesadjovi@outlook.fr>
2021-11-27 01:55:26 +0100

    Rename file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -24,4 +24,5 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 #endif 

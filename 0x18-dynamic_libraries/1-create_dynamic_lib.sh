acd032f71ea03d2959ec9ab4ffaa122f3c9e084a
pericles001 <periclesadjovi@outlook.fr>
2021-11-29 09:50:42 +0100

    Change code content

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,3 @@
-<<<<<<< HEAD:0x18-dynamic_libraries/create_lib.sh
-=======
 #!/usr/bin/bash
 gcc -Wall -c -fPIC *.c 
-gcc  -shared *.o  -o libdynamic.so 
->>>>>>> refs/remotes/origin/master:0x18-dynamic_libraries/1-create_dynamic_lib.sh
+gcc  -shared *.o  -o liball.so 

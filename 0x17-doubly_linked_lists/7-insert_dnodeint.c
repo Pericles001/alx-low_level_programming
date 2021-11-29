32bb6dfeb6624db83069cbe47b56fcec9d6b5344
pericles001 <periclesadjovi@outlook.fr>
2021-11-29 09:08:20 +0100

    Update files

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to head of list
+ * @idx: index of the list where the new node should be added
+ * @n: data of the new node
+ * 
+ * Return:  the address of the new node, or NULL if it failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}

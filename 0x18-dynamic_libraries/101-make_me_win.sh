32680303b536839fb7a85b87996a5b891d3cce5a
pericles001 <periclesadjovi@outlook.fr>
2021-12-01 15:08:08 +0100

    Submit solution

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 !/usr/bin/bash
-git clone https://github.com/holbertonschool/0x18.c.git ~
-LD_PRELOAD = ~/alx-low_level_programming/0x18-dynamic_libraries/winner.so
+git clone https://github.com/holbertonschool/0x18.c.git~
+LD_PRELOAD =~/alx-low_level_programming/0x18-dynamic_libraries/winner.so

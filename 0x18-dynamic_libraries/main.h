a4d4cd293d3cbed64dd0bdbde5a9ccbe629e2253
pericles001 <periclesadjovi@outlook.fr>
2021-12-01 03:02:35 +0100

    Update files

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -21,6 +21,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int nrandom();
 
 
 #endif /* __MAIN__ */

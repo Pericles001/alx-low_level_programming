01a49e14974fb04161dc3b2d17e4505ad1b535d6
pericles001 <periclesadjovi@outlook.fr>
2021-12-01 15:36:15 +0100

    Add random.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/alx-low_level_programming.git~
-LD_PRELOAD =~/alx-low_level_programming/0x18-dynamic_libraries/crack.so
+export LD_PRELOAD =~/alx-low_level_programming/0x18-dynamic_libraries/crack.so

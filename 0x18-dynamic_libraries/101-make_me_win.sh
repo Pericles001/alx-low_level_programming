6d5c8b53b9f6d25f93d475085389516a155d9c91
pericles001 <periclesadjovi@outlook.fr>
2021-12-01 15:48:11 +0100

    Update file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Pericles001/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so?raw=true
+wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD =/tmp/nrandom.so

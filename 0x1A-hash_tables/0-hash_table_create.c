0db82531cc26e3424fa76d8454ecc3fc2398bffb
Pericles001 <periclesadjovi@gmail.com>
2022-01-06 12:07:15 +0100

    Format code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,21 +9,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *ht;
+    hash_table_t *ht;
 
-ht = malloc(sizeof(hash_table_t));
-if (ht == NULL)
-return (NULL);
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
 
-ht->size = size;
+    ht->size = size;
 
-ht->array = malloc(sizeof(hash_table_t *)* size);
-if (ht->array == NULL)
-return NULL
+    ht->array = malloc(sizeof(hash_table_t *) * size);
+    if (ht->array == NULL)
+        return NULL
 
-for ( i = 0; i < size; i++)
-{
-ht->array[i] = NULL;
-}
-
+            for (i = 0; i < size; i++)
+        {
+            ht->array[i] = NULL;
+        }
 }

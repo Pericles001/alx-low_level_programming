5f32c0e6cb01227ddf0ff040d43c272aa710bbfb
Pericles001 <periclesadjovi@gmail.com>
2022-01-06 14:23:22 +0100

    Update indentation

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,14 @@
 #include "hash_tables.h"
 
 /**
- * key_index - function that gives the index of a key
- * @key: Key to be searched for
- * @size: Size of the hash table
- * 
- * Return: the index of the key
+ * key_index - Get the index at which a key/value pair should
+ *             be stored in array of a hash table.
+ * @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: The index of the key.
+ *
+ * Description: Uses the djb2 algorithm.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {

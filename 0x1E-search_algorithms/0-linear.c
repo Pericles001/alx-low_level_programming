7ef0f313eaabc38f1487eaa893ab0e5a62dd6024
pericles001 <periclesadjovi@outlook.fr>
2022-04-04 15:20:09 +0100

    Restore file*

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,19 +8,18 @@
  * Return: index of value or -1 if not found
  */
 
+
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t inc;
 
 	if (array == NULL)
 		return (-1);
-
-	for (i = 0; i < size; i++)
+	for (inc = 0; inc < size; inc++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
+		if (array[inc] == value)
+			return (inc);
 	}
-
 	return (-1);
 }

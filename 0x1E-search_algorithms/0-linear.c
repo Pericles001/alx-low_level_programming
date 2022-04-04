56af1883229218790c8955fdc86a17a22ee3e379
pericles001 <periclesadjovi@outlook.fr>
2022-04-04 14:54:44 +0100

    Done with task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in a sorted array of integers
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to search for
+ * Return: index of value or -1 if not found
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t inc;
+
+	if (array == NULL)
+		return (-1);
+	for (inc = 0; inc < size; inc++)
+	{
+		if (array[inc] == value)
+			return (inc);
+	}
+	return (-1);
+}

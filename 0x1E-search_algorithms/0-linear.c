f76844ca8851265bdc5efc9b7eb274458254b3a3
pericles001 <periclesadjovi@outlook.fr>
2022-04-04 15:05:12 +0100

    Update indentation

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,18 +8,17 @@
  * Return: index of value or -1 if not found
  */
 
-
 int linear_search(int *array, size_t size, int value)
 {
-size_t inc;
+	size_t inc;
 
-if (array == NULL)
-return (-1);
-for (inc = 0; inc < size; inc++)
-{
-printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
-if (array[inc] == value)
-return (inc);
-}
-return (-1);
+	if (array == NULL)
+		return (-1);
+	for (inc = 0; inc < size; inc++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
+		if (array[inc] == value)
+			return (inc);
+	}
+	return (-1);
 }

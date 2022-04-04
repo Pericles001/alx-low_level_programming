48c36942d797b70fe2e7e2abf7d17dd706973ffe
pericles001 <periclesadjovi@outlook.fr>
2022-04-04 14:56:51 +0100

    Added header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif

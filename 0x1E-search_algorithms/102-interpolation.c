fd49cc712b0a68df7de2c5f3110bac2f49481e33
pericles001 <periclesadjovi@outlook.fr>
2022-04-08 00:15:48 +0100

    Update file

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -16,7 +16,7 @@ int interpolation_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (l = 0; r = size - 1; r >= l;)
+	for (l = 0, (r = size - 1); r >= l;)
 	{
 		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
 		if (i < size)

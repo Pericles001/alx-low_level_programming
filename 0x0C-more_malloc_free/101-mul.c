bc5a2b1da7a8e3155cfe9d8c6a3ca92968ee7d38
pericles001 <periclesadjovi@outlook.fr>
2022-06-10 16:57:43 +0100

    Remove method declaration

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,6 @@
 
 #include "main.h"
 
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
 
 
 /**

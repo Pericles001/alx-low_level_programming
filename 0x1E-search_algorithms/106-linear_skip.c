46070a11d89bf296c85988b71952ed3d7211736f
pericles001 <periclesadjovi@outlook.fr>
2022-06-16 13:15:19 +0100

    Fix kernel style

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -14,33 +14,33 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *node, *jump;
+	skiplist_t *node, *jump;
 
-    if (list == NULL)
-        return (NULL);
+	if (list == NULL)
+		return (NULL);
 
-    for (node = jump = list; jump->next != NULL && jump->n < value;)
-    {
-        node = jump;
-        if (jump->express != NULL)
-        {
-            jump = jump->express;
-            printf("Value checked at index [%ld] = [%d]\n",
-                   jump->index, jump->n);
-        }
-        else
-        {
-            while (jump->next != NULL)
-                jump = jump->next;
-        }
-    }
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+				jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
 
-    printf("Value found between indexes [%ld] and [%ld]\n",
-           node->index, jump->index);
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-    for (; node->index < jump->index && node->n < value; node = node->next)
-        printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-    printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
-    return (node->n == value ? node : NULL);
+	return (node->n == value ? node : NULL);
 }

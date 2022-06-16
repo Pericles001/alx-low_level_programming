3574fd18572d4c9c0f4e1729bd4adddceb9e1bee
pericles001 <periclesadjovi@outlook.fr>
2022-06-16 13:12:15 +0100

    Fix kernel style

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -15,31 +15,31 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-    size_t step, step_size;
-    listint_t *node, *jump;
+	size_t step, step_size;
+	listint_t *node, *jump;
 
-    if (list == NULL || size == 0)
-        return (NULL);
+	if (list == NULL || size == 0)
+		return (NULL);
 
-    step = 0;
-    step_size = sqrt(size);
-    for (node = jump = list; jump->index + 1 < size && jump->n < value;)
-    {
-        node = jump;
-        for (step += step_size; jump->index < step; jump = jump->next)
-        {
-            if (jump->index + 1 == size)
-                break;
-        }
-        printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
-    }
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
+	{
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+	}
 
-    printf("Value found between indexes [%ld] and [%ld]\n",
-           node->index, jump->index);
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-    for (; node->index < jump->index && node->n < value; node = node->next)
-        printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-    printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
-    return (node->n == value ? node : NULL);
+	return (node->n == value ? node : NULL);
 }

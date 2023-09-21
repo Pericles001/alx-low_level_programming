5597744c06126620d55284f4f5543964dda50d89
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-09-21 12:03:06 +0100

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,44 +1,35 @@
 #include "main.h"
 
 /**
-* rot13 -  encodes a string using rot13
-* @str:the string targeted
-*Return: returns the encoded string
-*/
-
+ * rot13 - Encodes a string using ROT13.
+ * @str: The string to be encoded.
+ *
+ * Return: Returns the encoded string.
+ */
 char *rot13(char *str)
 {
-	int index1, index2;
+    int index1, index2;
+    char alphabet[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    char rot13key[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-											 'G', 'H', 'I', 'J', 'K', 'L',
-											 'M', 'N', 'O', 'P', 'Q', 'R',
-											 'S', 'T', 'U', 'V', 'W', 'X',
-											 'Y', 'Z', 'a', 'b', 'c', 'd',
-											 'e', 'f', 'g', 'h', 'i', 'j',
-											 'k', 'l', 'm', 'n', 'o', 'p',
-											 'q', 'r', 's', 't', 'u', 'v',
-											 'w', 'x', 'y', 'z'};
+    index1 = 0;
 
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-											 'T', 'U', 'V', 'W', 'X', 'Y',
-											 'Z', 'A', 'B', 'C', 'D', 'E',
-											 'F', 'G', 'H', 'I', 'J', 'K',
-											 'L', 'M', 'n', 'o', 'p', 'q',
-											 'r', 's', 't', 'u', 'v', 'w',
-											 'x', 'y', 'z', 'a', 'b', 'c',
-											 'd', 'e', 'f', 'g', 'h', 'i',
-											 'j', 'k', 'l', 'm'};
-	while (str[++index1])
-	{
-		for (index2 = 0; index2 < 52; index2++)
-		{
-			if (str[index1] == alphabet[index2])
-			{
-				str[index1] = rot13key[index2];
-				break;
-			}
-		}
-	}
-	return (str);
+    while (str[index1])
+    {
+        index2 = 0;
+
+        while (index2 < 52)
+        {
+            if (str[index1] == alphabet[index2])
+            {
+                str[index1] = rot13key[index2];
+                break;
+            }
+            index2++;
+        }
+
+        index1++;
+    }
+
+    return (str);
 }
